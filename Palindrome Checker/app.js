8ee47bf3641a4bc0055bcab7f371aa02e774fcd1
ibrahimjal <ibrahimjal20@gmail.com>
2022-03-03 20:01:02 +0000

    Added the JS

--- Palindrome Checker/app.js
+++ Palindrome Checker/app.js
@@ -1,5 +1,31 @@
 const input = document.getElementById("input")
 const checkBtn = document.getElementById("checkBtn")
-let description = document.getElementById("description")
 
+checkBtn.addEventListener("click", () => {
+    let text = input.value
+
+    checkPalindrome(text)
+    
+})
+
+function checkPalindrome(text){
+    let newText = text.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    console.log(text, newText) 
+    let len = newText.length;
+    let halfLen = Math.floor(len / 2)
+    let description = document.getElementById("description")
+    let i
+
+    for(i = 0; i < halfLen; i++){
+        if (newText[i] !== newText[len - 1 - i]){
+            description.textContent = "Nah!, this is not a Palindrome"
+            return;
+        }else{
+            description.textContent = "Yay!!! Its a Palindrome"
+        }
+    }
+
+   
+    
+}
 
